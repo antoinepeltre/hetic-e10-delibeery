447f7daa2d8e2d8cb9ffb5937a2ec2f2371764b0
Antoine Peltre <antoine.peltre@hetic.net>
2017-12-22 12:46:45 +0100

    [ADD] Panier Page

---
+++ app/src/main/java/com/hetic/antoine/hetic_project/CommandeActivity.java
@@ -0,0 +1,13 @@
+package com.hetic.antoine.hetic_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CommandeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_commande);
+    }
+}
