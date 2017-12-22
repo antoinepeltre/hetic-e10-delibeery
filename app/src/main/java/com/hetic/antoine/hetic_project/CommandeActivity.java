ef1337e28d2ef0076fbdc20f625cfc2bdb8d2aa9
Antoine Peltre <antoine.peltre@hetic.net>
2017-12-22 12:49:06 +0100

    [ADD] button navBar

--- app/src/main/java/com/hetic/antoine/hetic_project/CommandeActivity.java
+++ app/src/main/java/com/hetic/antoine/hetic_project/CommandeActivity.java
@@ -1,13 +1,46 @@
 package com.hetic.antoine.hetic_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CommandeActivity extends AppCompatActivity {
+    Button mButton;
+    Button tButton;
+    Button cButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_commande);
+
+        mButton = findViewById(R.id.profil);
+        mButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(CommandeActivity.this, ProfilActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        tButton = findViewById(R.id.table);
+        tButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(CommandeActivity.this, TableActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        cButton = findViewById(R.id.carte);
+        cButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(CommandeActivity.this, CartActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
